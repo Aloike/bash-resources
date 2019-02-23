a7f8b17b2037aa19ad9761f294abd1a4449337a3
GOM <contact@mathiasgo.fr>
2019-02-23 12:02:59 +0100

    Added aliases resources.

---
+++ aliases.sh
@@ -0,0 +1,7 @@
+# This file shall be included in your bashrc file ; for example :
+# FILEPATH_ALIASES="/home/<username>/scripts/bash/aliases.sh"
+# if [ -f ${FILEPATH_ALIASES} ]; then
+#     . ${FILEPATH_ALIASES}
+# fi
+
+alias c='clear'
