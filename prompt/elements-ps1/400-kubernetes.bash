9216f9af22fc63a8e72770439e2e52d2eea8d78b
Aloïké <aloike.go@gmail.com>
2020-12-15 16:46:58 +0100

    prompt/k8s: Implementation.

--- prompt/elements-ps1/400-kubernetes.bash
+++ prompt/elements-ps1/400-kubernetes.bash
@@ -4,40 +4,25 @@ PROMPT_PS1_FUNCTIONS+=("bash_prompt_command_kubernetes")
 
 function	bash_prompt_command_kubernetes()
 {
-	# local lGitStr=""
-	# local lGitPath="$(git rev-parse --show-toplevel 2>/dev/null)"
-	# if [ ! -z "${lGitPath}" ]
-	# then
-	# 	lGitLocalRepo=`basename ${lGitPath}`
-	# 	lGitBranch=`git branch|sed --quiet -e '/^\*/ s@\* @@p'`
+	if [ "${g_k8s_aliases_enabled}" != "1" ]
+	then
+		return
+	fi
 
-	# 	lGitPath=`pwd|sed -e 's@'"${lGitPath}"'@git:@' -e 's@/$@@'`
 
-		local lVar=""
-		lVar+="\n${_COL_CONTOUR}${C_BOX_DRAWINGS_LIGHT_VERTICAL_AND_RIGHT}"
+	local lVar=""
+	lVar+="\n${_COL_CONTOUR}${C_BOX_DRAWINGS_LIGHT_VERTICAL_AND_RIGHT}"
 
-		lVar+="${_COL_CONTOUR}${C_BOX_DRAWINGS_LIGHT_HORIZONTAL}["
-	# 	if [ "${g_git_aliases_enabled}" = "1" ]
-	# 	then
-	# 		lVar+="${FMT_BLD}${COL_BG_GRN}${COL_FG_BLK}"
-	# 	else
-	# 		lVar+="${FMT_BLD}${COL_FG_RED}"
-	# 	fi
-		lVar+="k8s${FMT_STD}"
-		lVar+="${_COL_CONTOUR}]"
+	lVar+="${_COL_CONTOUR}${C_BOX_DRAWINGS_LIGHT_HORIZONTAL}["
+	if [ "${g_k8s_aliases_enabled}" = "1" ]
+	then
+		# lVar+="${FMT_BLD}${COL_BG_GRN}${COL_FG_BLK}"
+        lVar+="${FMT_BLD}${COL_FG_GRN}"
+	else
+		lVar+="${FMT_BLD}${COL_FG_WHT}"
+	fi
+	lVar+="k8s${FMT_STD}"
+	lVar+="${_COL_CONTOUR}]"
 
-	#	lVar+="${_COL_CONTOUR}${C_BOX_DRAWINGS_LIGHT_HORIZONTAL}["
-	# 	lVar+="${COL_FG_GRN}${lGitLocalRepo}"
-	# 	lVar+="${_COL_CONTOUR}]"
-
-	#	lVar+="${_COL_CONTOUR}${C_BOX_DRAWINGS_LIGHT_HORIZONTAL}["
-	# 	lVar+="${COL_FG_YLW}${lGitBranch}"
-	# 	lVar+="${_COL_CONTOUR}]"
-
-	#	lVar+="${_COL_CONTOUR}${C_BOX_DRAWINGS_LIGHT_HORIZONTAL}["
-	# 	lVar+="${COL_FG_YLW}${lGitPath}"
-	# 	lVar+="${_COL_CONTOUR}]"
-
-		echo -en "${lVar}"
-	# fi
+	echo -en "${lVar}"
 }
