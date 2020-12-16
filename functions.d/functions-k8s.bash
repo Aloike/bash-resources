18e9d526871bf29573ad1a60484a6fff68e28e1f
Aloïké <aloike.go@gmail.com>
2020-12-16 09:09:50 +0000

    bash/k8s_aliases: Added autocompletion.

--- functions.d/functions-k8s.bash
+++ functions.d/functions-k8s.bash
@@ -8,9 +8,20 @@
 ##
 ## This function also exports the `g_k8s_aliases_enabled` variable set to `1`
 ## to let the environment know those aliases are available.
+##
+##  @par References
+##  + https://kubernetes.io/docs/tasks/tools/install-kubectl/#enable-kubectl-autocompletion
+# ##############################################################################
 function	k8s_aliases()
 {
+	# Source the completion script in your ~/.bashrc file
+	source <(kubectl completion bash)
+
+	# Create an alias for `kubectl`
 	alias k='kubectl'
+
+	# extend shell completion to work with that alias
+	complete -F __start_kubectl k
 
 	g_k8s_aliases_enabled=1
 	export g_k8s_aliases_enabled
