0cbe30301a4270456eca5e83d9a4e373227722b2
Aloïké <aloike.go@gmail.com>
2020-12-23 10:00:41 +0100

    bash/k8s_aliases: Renamed (with an uppercase) to ease access, and added the definition of the variable that enables the "k8s prompt element" (PS1).

--- functions.d/functions-k8s.bash
+++ functions.d/functions-k8s.bash
@@ -2,6 +2,7 @@
 
 # ##############################################################################
 # ##############################################################################
+##
 ##  @brief  This function declares short aliases for Kubernetes usage.
 ##
 ## Just call the function to declare various aliases.
@@ -9,10 +10,15 @@
 ## This function also exports the `g_k8s_aliases_enabled` variable set to `1`
 ## to let the environment know those aliases are available.
 ##
+## It also enables the "kubernetes prompt" from my custom prompts dir by setting
+## the `KUBE_PS1_ENABLED` variable to `on`.
+##
+##  @see    bash/resources/prompt/elements-ps1/400-kubernetes.bash
+##
 ##  @par References
 ##  + https://kubernetes.io/docs/tasks/tools/install-kubectl/#enable-kubectl-autocompletion
-# ##############################################################################
-function	k8s_aliases()
+##
+function	K8s_aliases()
 {
 	# Source the completion script in your ~/.bashrc file
 	source <(kubectl completion bash)
@@ -25,6 +31,9 @@ function	k8s_aliases()
 
 	g_k8s_aliases_enabled=1
 	export g_k8s_aliases_enabled
+
+	KUBE_PS1_ENABLED=on
+	export KUBE_PS1_ENABLED
 }
 
 # ##############################################################################
