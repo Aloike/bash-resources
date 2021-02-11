ebf04f55f6c9ca7db5f7bc1b85a43b2fe65a6dfc
Aloïké <aloike.go@gmail.com>
2021-02-11 17:37:01 +0100

    bash/resources: Updated the "installation" script to make it work from "anywhere".

--- 000-add_function_customPrompt_to_bashProfile.bash
+++ 000-add_function_customPrompt_to_bashProfile.bash
@@ -3,6 +3,8 @@
 
 FILE_BASH_RESOURCE="${HOME}/.bashrc"
 
+THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 
 # Check that there's no trace of the function to avoid multiple declarations
 if [ "$(grep 'FILEPATH_CUSTOM_RESOURCES' ${HOME}/.bash*|wc -l)" != "0" ]
@@ -20,7 +22,8 @@ cat << EOT >> "${FILE_BASH_RESOURCE}"
 # This function enables bash custom prompt.
 function	F_customPrompt_enable()
 {
-	FILEPATH_CUSTOM_RESOURCES="\${HOME}/scripts/bash/resources/resources.sh"
+	# FILEPATH_CUSTOM_RESOURCES="\${HOME}/scripts/bash/resources/resources.sh"
+	FILEPATH_CUSTOM_RESOURCES="__PLACEHOLDER_PATH_TO_CUSTOM_RESOURCES__"
 	if [ ! -f "\${FILEPATH_CUSTOM_RESOURCES}" ]
 	then
 		echo "No custom bash resources file available at '\${FILEPATH_CUSTOM_RESOURCES}'."
@@ -29,6 +32,11 @@ function	F_customPrompt_enable()
 	fi
 }
 EOT
+
+sed --in-place \
+	-e 's@__PLACEHOLDER_PATH_TO_CUSTOM_RESOURCES__@'"${THIS_SCRIPTDIR}/resources.sh"'@' \
+	"${FILE_BASH_RESOURCE}"
+
 
 
 echo "Now reload your bash resources, for example by doing..."
